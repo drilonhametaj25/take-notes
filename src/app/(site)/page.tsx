2d4511dc766f9e2c74a71bd642d5ecb4de2b4ce8
drilon <drilonhametaj25@gmail.com>
2024-04-02 15:03:51 +0200

    add appBanner.png

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,6 +1,8 @@
-import TitleSeciton from "@/components/landing-page/title-section";
+import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import React from "react";
+import Banner from '../../../public/appBanner.png'
 
 const HomePage = () => {
     return (
@@ -15,10 +17,11 @@ const HomePage = () => {
                 md:justify-center
                 md:items-start
             '>
-                <TitleSeciton pill="Your workspace, Perfected" title='All-In-One Collaboration and Productivity Platform'/>
+                <TitleSection pill="Your workspace, Perfected" title='All-In-One Collaboration and Productivity Platform'/>
                 <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
                     <Button variant="btn-secondary" className="w-full rounded-[10px] p-6 text-2xl bg-background">Get It FREE</Button>
                 </div>
+                <div className="md:mt-[-90px] sm:w-full w-[750px] justify-center items-center mt-[-40px] sm:ml-0 ml-[-50px]"><Image src={Banner} alt='Application Banner' /></div>
             </div>
         </section>
     )
