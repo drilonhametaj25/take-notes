ef1fb9a6db0487cb3f22568d2a66f9b24a75f267
drilon <drilonhametaj25@gmail.com>
2024-04-02 15:11:26 +0200

    fix some layout alignment

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -15,10 +15,10 @@ const HomePage = () => {
                 sm:flex-col
                 gap-4
                 md:justify-center
-                md:items-start
+                justify-center
             '>
                 <TitleSection pill="Your workspace, Perfected" title='All-In-One Collaboration and Productivity Platform'/>
-                <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
+                <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px] justify-center">
                     <Button variant="btn-secondary" className="w-full rounded-[10px] p-6 text-2xl bg-background">Get It FREE</Button>
                 </div>
                 <div className="md:mt-[-90px] sm:w-full w-[750px] justify-center items-center mt-[-40px] sm:ml-0 ml-[-50px]"><Image src={Banner} alt='Application Banner' />
