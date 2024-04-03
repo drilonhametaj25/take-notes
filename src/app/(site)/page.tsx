d96fe84c8373929414d3939e7d6eae815338a15c
drilon <drilonhametaj25@gmail.com>
2024-04-03 09:37:13 +0200

    add all image and icon

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,34 +6,41 @@ import Banner from '../../../public/appBanner.png'
 
 const HomePage = () => {
     return (
-        <section>
-            <div className='overflow-hidden
-                px-4
-                sm:px-6
-                mt-6
-                sm:flex
-                sm:flex-col
-                gap-4
-                md:justify-center
-                justify-center
-            '>
-                <TitleSection pill="Your workspace, Perfected" title='All-In-One Collaboration and Productivity Platform'/>
-                <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px] justify-center">
-                    <Button variant="btn-secondary" className="w-full rounded-[10px] p-6 text-2xl bg-background">Get It FREE</Button>
-                </div>
-                <div className="md:mt-[-90px] sm:w-full w-[750px] justify-center items-center mt-[-40px] sm:ml-0 ml-[-50px]"><Image src={Banner} alt='Application Banner' />
-                    <div className="
-                        bottom-0
-                        top-[50%]
-                        bg-gradient-to-t
-                        dark:from-background
-                        left-0
-                        right-0
-                        z-10
-                    "></div>
-                </div>
-            </div>
-        </section>
+        <>
+            <section className='overflow-hidden
+            px-4
+            sm:px-6
+            mt-6
+            sm:flex
+            sm:flex-col
+            gap-4
+            md:justify-center
+            justify-center
+                '>
+                    <TitleSection pill="✨ Your workspace, Perfected" title='All-In-One Collaboration and Productivity Platform'/>
+                    <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px] justify-center">
+                        <Button variant="btn-secondary" className="w-full rounded-[10px] p-6 text-2xl bg-background">Get It FREE</Button>
+                    </div>
+                    <div className="md:mt-[-90px] sm:w-full w-[750px] justify-center items-center mt-[-40px] sm:ml-0 ml-[-50px]"><Image src={Banner} alt='Application Banner' />
+                        <div className="
+                            bottom-0
+                            top-[50%]
+                            bg-gradient-to-t
+                            dark:from-background
+                            left-0
+                            right-0
+                            z-10
+                        "></div>
+                    </div>
+                    <div className="relative">
+
+                    </div>
+            </section>
+            <section>
+                <div></div>
+            </section>
+        </>
+        
     )
 }
 
