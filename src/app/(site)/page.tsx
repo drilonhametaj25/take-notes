24fb81f47ff36f3eeb2eb3648c042e85860cc29b
drilon <drilonhametaj25@gmail.com>
2024-04-03 09:56:42 +0200

    animation to icons

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,12 +2,14 @@ import TitleSection from "@/components/landing-page/title-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
-import Banner from '../../../public/appBanner.png'
+import Banner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 const HomePage = () => {
-    return (
-        <>
-            <section className='overflow-hidden
+  return (
+    <>
+      <section
+        className="overflow-hidden
             px-4
             sm:px-6
             mt-6
@@ -16,13 +18,24 @@ const HomePage = () => {
             gap-4
             md:justify-center
             justify-center
-                '>
-                    <TitleSection pill="✨ Your workspace, Perfected" title='All-In-One Collaboration and Productivity Platform'/>
-                    <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px] justify-center">
-                        <Button variant="btn-secondary" className="w-full rounded-[10px] p-6 text-2xl bg-background">Get It FREE</Button>
-                    </div>
-                    <div className="md:mt-[-90px] sm:w-full w-[750px] justify-center items-center mt-[-40px] sm:ml-0 ml-[-50px]"><Image src={Banner} alt='Application Banner' />
-                        <div className="
+                "
+      >
+        <TitleSection
+          pill="✨ Your workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        <div className="bg-white p-[2px] mt-[6] rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px] justify-center">
+          <Button
+            variant="btn-secondary"
+            className="w-full rounded-[10px] p-6 text-2xl bg-background"
+          >
+            Get It FREE
+          </Button>
+        </div>
+        <div className="md:mt-[-90px] sm:w-full w-[750px] justify-center items-center mt-[-40px] sm:ml-0 ml-[-50px]">
+          <Image src={Banner} alt="Application Banner" />
+          <div
+            className="
                             bottom-0
                             top-[50%]
                             bg-gradient-to-t
@@ -30,18 +43,53 @@ const HomePage = () => {
                             left-0
                             right-0
                             z-10
-                        "></div>
-                    </div>
-                    <div className="relative">
+                        "
+          ></div>
+        </div>
+      </section>
+      <section className="relative">
+        <div
+          className="overflow-hidden 
+                flex 
+                absolute 
+                after:content[''] 
+                after:dark:from-brand-dark 
+                after:to-transparent 
+                after:from-background 
+                after:bg-grandient-to-l 
+                after:right-0 
+                after:top-0 
+                after:bottom-0 
+                after:w-20 
+                after:z-10
+                after:absolute
 
-                    </div>
-            </section>
-            <section>
-                <div></div>
-            </section>
-        </>
-        
-    )
-}
+                before:content[''] 
+                before:dark:from-brand-dark 
+                before:to-transparent 
+                before:from-background 
+                before:bg-grandient-to-r
+                before:left-0 
+                before:top-0 
+                before:bottom-0 
+                before:w-20 
+                before:z-10
+                before:absolute
+                "
+        >
+            {[...Array(2)].map((arr) => (
+                <div key={arr} className="flex flex-nowrap animate-slide">
+                    {CLIENTS.map((client) => (
+                        <div key={client.alt} className="relative w-[200px] m-20 shrink-0 flex items-center">
+                            <Image src={client.logo} alt={client.alt} width={200} className="object-contain max-w-none"/>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
