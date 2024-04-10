d691a1643dc8b0f101cdb84a9373241edb9eaf7c
drilon <drilonhametaj25@gmail.com>
2024-04-10 12:33:45 +0200

    login form with auth actions

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,10 +5,14 @@ import {SubmitHandler, useForm } from "react-hook-form"
 import * as z from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FormSchema } from "@/lib/types";
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from '../../../../public/cypresslogo.svg';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/global/Loader";
+import { actionLoginUser } from "@/lib/server-actions/auth-actions";
 
 
 const LoginPage = () => {
@@ -21,9 +25,14 @@ const LoginPage = () => {
         defaultValues: {email: '', password: ''}
     });
 
-    const isLoaded = form.formState.isSubmitting
-    const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = (formData) => {
-
+    const isLoading = form.formState.isSubmitting
+    const onSubmit: SubmitHandler<z.infer<typeof FormSchema>> = async (formData) => {
+        const {error} = await actionLoginUser(formData)
+        if(error){
+            form.reset();
+            setSubmitError(error.message)
+        }
+        router.replace('/dashboard')
     }
     
     return <Form {...form}>
@@ -45,6 +54,64 @@ const LoginPage = () => {
                     Take Notes
                 </span>
             </Link>
+            <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <Button
+          type="submit"
+          className="w-full p-6"
+          size="lg"
+          disabled={isLoading}
+        >
+          {!isLoading ? 'Login' : <Loader />}
+        </Button>
+        <span className="self-container">
+          Dont have an account?{' '}
+          <Link
+            href="/signup"
+            className="text-primary"
+          >
+            Sign Up
+          </Link>
+        </span>
         </form>
     </Form>
 }
