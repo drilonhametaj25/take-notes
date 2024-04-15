0358d53b33781356baeae9c6d9a8335e076f42d4
drilon <drilonhametaj25@gmail.com>
2024-04-15 14:11:49 +0200

    add state variable

--- src/app/(auth)/singup/page.tsx
+++ src/app/(auth)/singup/page.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useMemo, useState } from "react";
 import { z } from "zod";
 
-const signUpFormSchema = z.object({
-    email: z.string().describe('Email').email({message: 'Invalid email'}),
-    password: z.string().describe('Password').min(6, 'Password must be minimum 6 characters'),
-    confirmPassword: z.string().describe('Confirm Password').min(6, 'Password must be minimum 6 characters')
-    
-}).refine((data) => data.password === data.confirmPassword, {message: "Password don't match!", path: ['confirmPassword']} )
+const signUpFormSchema = z
+  .object({
+    email: z.string().describe("Email").email({ message: "Invalid email" }),
+    password: z
+      .string()
+      .describe("Password")
+      .min(6, "Password must be minimum 6 characters"),
+    confirmPassword: z
+      .string()
+      .describe("Confirm Password")
+      .min(6, "Password must be minimum 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match!",
+    path: ["confirmPassword"],
+  });
 
 const SignUp = () => {
-    return <></>
-}
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [submitError, setSubmitError] = useState('')
+  const [confirmation, setConfirmation] = useState(false)
+
+  const constExchangeError = useMemo(() => {
+    if (!searchParams) return "";
+    return searchParams.get('error_description')
+  }, [searchParams]);
+
+  const confirmationAndErrorStyles = useMemo(() => {}, [])
+
+  return <></>;
+};
 
 export default SignUp;
