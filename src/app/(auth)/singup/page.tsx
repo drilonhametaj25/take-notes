a0b4ae104103af12e500c395b32680db2945dd48
drilon <drilonhametaj25@gmail.com>
2024-04-15 14:04:00 +0200

    from field with control

--- src/app/(auth)/singup/page.tsx
+++ src/app/(auth)/singup/page.tsx
@@ -3,6 +3,8 @@ import { z } from "zod";
 
 const signUpFormSchema = z.object({
     email: z.string().describe('Email').email({message: 'Invalid email'}),
+    password: z.string().describe('Password').min(6, 'Password must be minimum 6 characters'),
+    confirmPassword: z.string().describe('Confirm Password').min(6, 'Password must be minimum 6 characters')
     
 })
 
