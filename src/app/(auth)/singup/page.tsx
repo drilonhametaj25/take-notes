469e7ae948e736c623370f6b5b29cb03fb741c70
drilon <drilonhametaj25@gmail.com>
2024-04-16 12:23:32 +0200

    registration form

--- src/app/(auth)/singup/page.tsx
+++ src/app/(auth)/singup/page.tsx
@@ -1,6 +1,13 @@
 'use client';
+import Loader from "@/components/global/Loader";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import clsx from "clsx";
+import Image from "next/image";
+import Link from "next/link";
+import Logo from '../../../../public/cypresslogo.svg';
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useMemo, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -45,10 +52,106 @@ const SignUp = () => {
     resolver: zodResolver(signUpFormSchema),
     defaultValues: {email: '', password: '', confirmPassword: ''}
   })
+  const isLoading = form.formState.isSubmitting;
+  const onSubmit = () => {}
 
-  
+  const singUpHandler = () => {}
+ 
+  return <Form {...form}>
+    <form onChange={() => {if(submitError) setSubmitError('')}}
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="w-full sm:justify-center sm-w-[400px] space-y-6 flex flex-col"
+    >
+<Link 
+            href="/"
+            className="w-full flex justify-left items-center"
+            >
+                <Image
+                src={Logo}
+                alt="Logo"
+                width={50}
+                height={50}
+                />
+                <span className="font-semibold dark:text-white text-4xl first-letter:ml2">
+                    Take Notes
+                </span>
+            </Link>
+            <FormDescription
+          className="
+        text-foreground/60"
+        >
+          An all-In-One Collaboration and Productivity Platform
+        </FormDescription>
+        {!confirmation && !constExchangeError && <>
+          <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="Confirm Password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          <Button type="submit" className="w-full p-6" disabled={isLoading}>
+            {!isLoading ? 'Create Account' : <Loader/>}
 
-  return <></>;
+          </Button>
+        </>}
+        
+        {submitError && <FormMessage>{submitError}</FormMessage>}
+        <span className="self-container">
+          Already have an account?{' '}
+          <Link
+            href="/login"
+            className="text-primary"
+          >
+            Login
+          </Link>
+        </span>
+    </form>
+  </Form>;
 };
 
 export default SignUp;
