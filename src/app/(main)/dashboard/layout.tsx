7178e8d4fbbdccb1faf307f522339aea0bc97d7f
drilon <drilonhametaj25@gmail.com>
2024-04-22 09:37:48 +0200

    init dashboard section

---
+++ src/app/(main)/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface LayoutProps {
+    children: React.ReactNode,
+    params: any
+}
+
+const Layout: React.FC<LayoutProps> = () => {
+    return <div></div>
+}
+
+export default Layout
