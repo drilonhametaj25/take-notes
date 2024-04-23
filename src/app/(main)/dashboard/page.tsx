53553abc72172fc711f0b810cd0b35acaf2d9727
drilon <drilonhametaj25@gmail.com>
2024-04-23 09:35:47 +0200

    get workspace info

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,17 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import React from "react";
+import { cookies } from "next/headers";
+import db from "@/lib/supabase/db";
 
-const Page = () => {
-    return <div>Page</div>
+const DashboardPage = async () => {
+    const supabase = createServerComponentClient({cookies})
+
+    const {data: {user}} = await supabase.auth.getUser()
+    if(!user) return;
+
+    const workspace = await db.query.workspaces.findFirst({where: (workspace, {eq}) => eq(workspace.workspaceOwner, user.id)})
+
+    return <div>DashboardPage</div>
 }
 
-export default Page
+export default DashboardPage
