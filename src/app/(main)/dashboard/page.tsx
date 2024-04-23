586e34c85da2222237fe427d75c92cda8451e791
drilon <drilonhametaj25@gmail.com>
2024-04-23 09:56:59 +0200

    supabase getSubscription

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { cookies } from "next/headers";
 import db from "@/lib/supabase/db";
 import { redirect } from "next/navigation";
 import DashboardSetup from "@/components/dashboard-setup/dashboard-setup";
+import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
 
 const DashboardPage = async () => {
     const supabase = createServerComponentClient({cookies})
@@ -11,9 +12,14 @@ const DashboardPage = async () => {
     const {data: {user}} = await supabase.auth.getUser()
     if(!user) return;
     const workspace = await db.query.workspaces.findFirst({where: (workspace, {eq}) => eq(workspace.workspaceOwner, user.id)})
-    if(!workspace) return (<div className="bg-background h-screen w-screen flex justify-center items-center">
-        <DashboardSetup subscription={} user={}></DashboardSetup>
-    </div>);
+    const { data: subscription, error: subsctiptionError} = await getUserSubscriptionStatus(user.id)
+    if(!workspace) {
+        return (
+            <div className="bg-background h-screen w-screen flex justify-center items-center">
+                <DashboardSetup subscription={subscription} user={user}></DashboardSetup>
+            </div>
+        );
+    }
     redirect(`/dashboard/${workspace.id}`)
 }
 
---
+++ src/lib/supabase/queries.ts
@@ -0,0 +1,19 @@
+import db from "./db";
+import { Subscription } from "./supabase.types";
+
+export const getUserSubscriptionStatus = async (userId: string) => {
+  try {
+    const data = await db.query.subscriptions.findFirst({
+      where: (s, { eq }) => eq(s.userId, userId),
+    });
+
+    if (data) {
+      return { data: data as Subscription, error: null };
+    } else {
+        return { data: null, error: "Error" };
+    }
+  } catch (error) {
+    console.error(error);
+    return { data: null, error: "Error" };
+  }
+};
