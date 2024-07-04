a5253c9a1a465f3e4fdfcfb89d3dc02d6699400c
drilon <drilonhametaj25@gmail.com>
2024-07-04 14:13:22 +0200

    feat: add sidebar

--- src/app/(main)/dashboard/[workspaceid]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceid]/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/sidebar/sidebar";
 import React from "react";
 
 interface LayoutProps {
@@ -6,9 +7,12 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({children, params}) => {
-    return <div >
-        {children}
-    </div>
+    return <main className="flex overflow-hidden h-screen w-screen">
+        <Sidebar params={params}/>
+        <div className="dark:border-Neutrals-12/70 border-l-[1px] w-full relative overflow-scroll">
+            {children}
+        </div>
+    </main>
 }
 
 export default Layout
